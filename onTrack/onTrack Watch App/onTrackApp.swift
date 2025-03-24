882bf3d381db1fb2697677c2aff42c6e74429100
Lilyan Toma <lilyan.toma@icloud.com>
2025-03-24 10:59:22 -0400

    hi

---
+++ onTrack/onTrack Watch App/onTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  onTrackApp.swift
+//  onTrack Watch App
+//
+//  Created by Lilly Toma on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct onTrack_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ onTrack/onTrack Watch AppTests/onTrack_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  onTrack_Watch_AppTests.swift
+//  onTrack Watch AppTests
+//
+//  Created by Lilly Toma on 3/24/25.
+//
+
+import Testing
+@testable import onTrack_Watch_App
+
+struct onTrack_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
