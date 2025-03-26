31204a8608cdfaba2798ee65779759009ebb9ee1
Lilyan Toma <lilyan.toma@icloud.com>
2025-03-26 08:58:13 -0400

    moved pages around

---
+++ onTrack/onTrack Watch App/SummaryPage.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryPage.swift
+//  onTrack Watch App
+//
+//  Created by Lilly Toma on 3/26/25.
+//
+
+import SwiftUI
+
+struct SummaryPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryPage()
+}
